88f3f013ecf06e3ea2e8cca5bdc699bc2ca8f1b2
Daniel Devesa <dadederk@icloud.com>
2024-08-21 22:47:11 +0100

    Create initial projects for both backend and iOS client.

---
+++ a11y-tracker-iOS/A11yTracker/A11yTracker/A11yTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  A11yTrackerApp.swift
+//  A11yTracker
+//
+//  Created by Daniel Devesa Derksen-Staats on 21/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct A11yTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
