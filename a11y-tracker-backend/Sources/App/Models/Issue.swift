58579eacaf2f4ae50f66fbe150bd9ed62d4b2bb8
Daniel Devesa <dadederk@icloud.com>
2024-08-24 19:04:49 +0100

    Created an Issue model and an Issues Query model to be able to choose the sort order for the issues requested. Created a get Issues route, that can pass an app id in the url. And a post Issue route to be able to create a new issue.

---
+++ a11y-tracker-backend/Sources/App/Models/Issue.swift
@@ -0,0 +1,13 @@
+//
+//  Issue.swift
+//  
+//
+//  Created by Daniel Devesa Derksen-Staats on 24/08/2024.
+//
+
+import Foundation
+import Vapor
+
+struct Issue: Content {
+    let title: String
+}
--- a11y-tracker-backend/Sources/App/routes.swift
+++ a11y-tracker-backend/Sources/App/routes.swift
@@ -2,12 +2,47 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+    //MARK: Groups
+    let issues = app.grouped("issues")
+    let issue = app.grouped("issue")
+    
+    //MARK: Issues
+    issues.get(":appId") { req async throws in
+        let appId = req.parameters.get("appId")
+        let issuesQuery = try req.query.decode(IssuesQuery.self)
+        
+        var forAppId = ""
+        
+        if let appId = appId {
+            forAppId = " for \(appId)"
+        }
+        
+        if let sortOrder = issuesQuery.sortOrder {
+            switch sortOrder {
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+            case .mostRecent:
+                return [
+                    Issue(title: "The most recent\(forAppId)"),
+                    Issue(title: "The second most recent\(forAppId)"),
+                ]
+            case .mostPopular:
+                return [
+                    Issue(title: "The most popular\(forAppId)"),
+                    Issue(title: "The second most popular\(forAppId)"),
+                ]
+            }
+        }
+        
+        return [
+            Issue(title: "The first created issue\(forAppId)"),
+            Issue(title: "The second created issue\(forAppId)"),
+        ]
+    }
+    
+    //MARK: Issue    
+    issue.post("issue") { req async throws in
+        let issue = try req.content.decode(Issue.self)
+        return issue
     }
 
     try app.register(collection: TodoController())
