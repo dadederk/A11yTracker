8f8dfe17fe882875783183318eaa6d6b89eecf5f
Dani Devesa <dadederk@icloud.com>
2024-08-25 14:47:03 +0100

    Add possibility for getting an Issue by id, and deleting an issue by id.

--- a11y-tracker-backend/Sources/App/Controllers/IssueController.swift
+++ a11y-tracker-backend/Sources/App/Controllers/IssueController.swift
@@ -10,14 +10,36 @@ import Vapor
 
 struct IssueController: RouteCollection {
     func boot(routes: any Vapor.RoutesBuilder) throws {
-        let issue = routes.grouped(AuthMiddleware()).grouped("issue")
-        issue.post(use: create)
+        let issue = routes.grouped("issue")
+        issue.grouped(AuthMiddleware()).post(use: create)
+        issue.grouped(AuthMiddleware()).delete(":id", use: delete)
+        issue.get(":id", use: index)
     }
     
     @Sendable
     func create(req: Request) async throws -> Issue {
         let issue = try req.content.decode(Issue.self)
         try await issue.save(on: req.db)
+        return issue
+    }
+
+    @Sendable
+    func index(req: Request) async throws -> Issue {
+        guard let issue = try await Issue.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        return issue
+    }
+    
+    @Sendable
+    func delete(req: Request) async throws -> Issue {
+        guard let issue = try await Issue.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        try await issue.delete(on: req.db)
+        
         return issue
     }
 }
